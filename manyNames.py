aa13d404d106916cbee13896c8b144e4191d63c4
h4llow3En <mail@felixdoering.com>
2016-10-18 13:54:29 +0200

    add missing else

--- manyNames.py
+++ manyNames.py
@@ -24,7 +24,7 @@ def has_droggelbecher(bot_stuff):
 def is_mm(bot_stuff):
     realy = 'Och ne, wirklich? {}?'
     has_mm = True if mm(bot_stuff['message']) is not None else False
-    has_herz = True if herzog(bot_stuff['message']) is not None False
+    has_herz = True if herzog(bot_stuff['message']) is not None else False
     if has_mm or has_herz:
         if has_mm != has_herz:
             bot_stuff['sendMessage'](chat_id=bot_stuff['chat_id'],
